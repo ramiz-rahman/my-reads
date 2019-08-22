35609782749fec3d2239fed25237e429716dc2b7
ramiz-rahman <ramiz.rahman@outlook.com>
2019-08-23 01:44:31 +0600

    feat: Create SearchPage component for '/search' path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './Homepage';
+import SearchPage from './SearchPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={Homepage} />
+        <Route exact path="/search" component={SearchPage} />
       </div>
     );
   }
---
+++ src/SearchPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SearchPage extends Component {
+  render() {
+    return <div>Search Page</div>;
+  }
+}
+
+export default SearchPage;
