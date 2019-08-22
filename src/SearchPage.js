cf7a1691ad9bbe011de4d66546d75f951cd5d21c
ramiz-rahman <ramiz.rahman@outlook.com>
2019-08-23 02:41:32 +0600

    feat: display all books fetched using search query in Searchpage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
+import Bookshelf from './Bookshelf';
 
 class SearchPage extends Component {
+  state = {
+    query: 'Art',
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.search(this.state.query).then((books) =>
+      this.setState({ books })
+    );
+  }
   render() {
-    return <div>Search Page</div>;
+    return (
+      <div>
+        {this.state.books && (
+          <Bookshelf
+            books={this.state.books}
+            shelves={['heLLO', 'FuckYOU', 'MOther']}
+          />
+        )}
+      </div>
+    );
   }
 }
 
