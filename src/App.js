5f66b4293beb9f47cee30630298c80451e2b8242
ramiz-rahman <ramiz.rahman@outlook.com>
2019-08-23 01:40:56 +0600

    refactor: Create homepage container component to represent app homepage

--- src/App.js
+++ src/App.js
@@ -1,74 +1,14 @@
 import React, { Component } from 'react';
 import * as BooksAPI from './BooksAPI';
+import { Route } from 'react-router-dom';
 import './App.css';
-import Bookshelf from './Bookshelf';
+import Homepage from './Homepage';
 
 class App extends Component {
-  state = {
-    books: []
-  };
-
-  async componentDidMount() {
-    const books = await BooksAPI.getAll();
-    if (books) this.setState({ books });
-    //BooksAPI.getAll().then((books) => this.setState({ books }));
-  }
-
-  handleMove = (e, id, newShelf) => {
-    e.preventDefault();
-    const book = this.state.books.find((book) => book.id === id);
-    BooksAPI.update(book, newShelf).then(
-      this.setState((prevState) => {
-        return {
-          books: prevState.books.map((book) => {
-            if (book.id === id) {
-              book.shelf = newShelf;
-            }
-            return book;
-          })
-        };
-      })
-    );
-  };
-
   render() {
-    console.log(this.state.books[0]);
-    const shelves = Array.from(
-      this.state.books.reduce((shelves, book) => {
-        shelves.add(book.shelf);
-        return shelves;
-      }, new Set())
-    );
-    console.log(shelves);
     return (
       <div className="App">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <Bookshelf
-          title="Currently Reading"
-          books={this.state.books.filter(
-            (book) => book.shelf === 'currentlyReading'
-          )}
-          shelves={shelves}
-          onMove={this.handleMove}
-        />
-        <Bookshelf
-          title="Want to Read"
-          books={this.state.books.filter(
-            (book) => book.shelf === 'wantToRead'
-          )}
-          shelves={shelves}
-          onMove={this.handleMove}
-        />
-        <Bookshelf
-          title="Read"
-          books={this.state.books.filter(
-            (book) => book.shelf === 'read'
-          )}
-          shelves={shelves}
-          onMove={this.handleMove}
-        />
+        <Route exact path="/" component={Homepage} />
       </div>
     );
   }
---
+++ src/Homepage.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
+import Bookshelf from './Bookshelf';
+
+class Homepage extends Component {
+  state = {
+    books: []
+  };
+
+  async componentDidMount() {
+    const books = await BooksAPI.getAll();
+    if (books) this.setState({ books });
+    //BooksAPI.getAll().then((books) => this.setState({ books }));
+  }
+
+  handleMove = (e, id, newShelf) => {
+    e.preventDefault();
+    const book = this.state.books.find((book) => book.id === id);
+    BooksAPI.update(book, newShelf).then(
+      this.setState((prevState) => {
+        return {
+          books: prevState.books.map((book) => {
+            if (book.id === id) {
+              book.shelf = newShelf;
+            }
+            return book;
+          })
+        };
+      })
+    );
+  };
+
+  render() {
+    const shelves = Array.from(
+      this.state.books.reduce((shelves, book) => {
+        shelves.add(book.shelf);
+        return shelves;
+      }, new Set())
+    );
+
+    return (
+      <div>
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <Bookshelf
+          title="Currently Reading"
+          books={this.state.books.filter(
+            (book) => book.shelf === 'currentlyReading'
+          )}
+          shelves={shelves}
+          onMove={this.handleMove}
+        />
+        <Bookshelf
+          title="Want to Read"
+          books={this.state.books.filter(
+            (book) => book.shelf === 'wantToRead'
+          )}
+          shelves={shelves}
+          onMove={this.handleMove}
+        />
+        <Bookshelf
+          title="Read"
+          books={this.state.books.filter(
+            (book) => book.shelf === 'read'
+          )}
+          shelves={shelves}
+          onMove={this.handleMove}
+        />
+      </div>
+    );
+  }
+}
+
+export default Homepage;
